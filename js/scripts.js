efa6a39fd00bdadfad0a5a5e1929b5af9be13fdd
Ana <ana.toma92@gmail.com>
2017-06-13 22:03:10 +0300

    complete portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ offsetLap();
 // particles
 
 var options = {
-	particleColor: '#1F2833',
+	particleColor: '#4e627a',
 	background: '#0B0C10',
 	interactive: true,
 	speed: 'slow',
@@ -54,5 +54,44 @@ $(document).scroll(function() {
 $('#top').on('click',function () {
   $('#top').removeClass('show');
 });
+
+//SMOOTH SCROLL
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 });
